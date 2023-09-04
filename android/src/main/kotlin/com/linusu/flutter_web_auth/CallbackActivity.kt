4822c4320973fd1a5fcc895a23012cc17606f676
Jérémie Yardin <jeremie_yardin@ext.connect-tech.sncf>
2023-09-04 15:18:34 +0200

    Fix: in some cases (app with launchMode=singleTop), the custom tab was kept open after the deeplink

--- android/src/main/kotlin/com/linusu/flutter_web_auth/CallbackActivity.kt
+++ android/src/main/kotlin/com/linusu/flutter_web_auth/CallbackActivity.kt
@@ -1,8 +1,8 @@
 package com.linusu.flutter_web_auth
 
 import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 
 class CallbackActivity: Activity() {
@@ -19,6 +19,8 @@ class CallbackActivity: Activity() {
     val context = applicationContext
     val pm: PackageManager = context.packageManager
     val intent = pm.getLaunchIntentForPackage(context.packageName)
+    // Makes sure the custom tab will be closed
+    intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
     startActivity(intent)
     finish()
   }
